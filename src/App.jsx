f0c5b41682e4660a3d4ae9b245bdd7b20d2672d9
m-alex-n <alexnzioka42@gmail.com>
2025-03-28 13:08:52 +0300

    added analytics for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import MemoryGame from "./components/memory-game";
-import TailwindTest from "./components/TailwindTest"
+import TailwindTest from "./components/TailwindTest";
 
 function App() {
   return (
     <div>
-    {/* <TailwindTest /> */}
-  <div className="App">
-  <MemoryGame />;
-  </div>
+      {/* <TailwindTest /> */}
+      <div className="App">
+        <MemoryGame />;
+      </div>
+      <Analytics />
     </div>
-  )
+  );
 }
 
 export default App;
